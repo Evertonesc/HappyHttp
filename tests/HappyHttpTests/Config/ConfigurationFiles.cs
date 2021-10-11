bdf7e47288176d2dc53704d31b0b8ee03dd0b0b0
evertonesc <evertonsouzaesc@gmail.com>
2021-10-11 15:31:47 -0300

    test: Adding load config files method

---
+++ tests/HappyHttpTests/Config/ConfigurationFiles.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HappyHttpTests.Config
+{
+    public static class ConfigurationFiles
+    {
+        public static IConfiguration ReadAppSettings()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            return config;
+        }
+    }
+}
