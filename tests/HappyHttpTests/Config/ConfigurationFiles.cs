07a92438323e520e736454f9896815ff6d60ca2e
evertonesc <evertonsouzaesc@gmail.com>
2021-11-08 19:10:42 -0300

    refactor: Updating to .NET 6 and using File-scoped namespaces

--- tests/HappyHttpTests/Config/ConfigurationFiles.cs
+++ tests/HappyHttpTests/Config/ConfigurationFiles.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-namespace HappyHttpTests.Config
-{
-    public static class ConfigurationFiles
-    {
-        public static IConfiguration ReadAppSettings()
-        {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.test.json", optional: false, reloadOnChange: true)
-                .Build();
+namespace HappyHttpTests.Config;
 
-            return config;
-        }
+public static class ConfigurationFiles
+{
+    public static IConfiguration ReadAppSettings()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.test.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return config;
     }
 }
